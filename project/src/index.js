69e7ab115aa51e1975a42c97f84d1132cb1ff7e2
Minh Thinh <115991092+MinhThinhdarius@users.noreply.github.com>
2024-09-10 23:46:42 +0700

    add template

---
+++ project/src/index.js
@@ -0,0 +1,31 @@
+import "./bootstrap-custom.css";
+import "./index.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { HashRouter as Router } from "react-router-dom";
+import Test from "./Test";
+import reportWebVitals from "./reportWebVitals";
+
+library.add(fas, far, fab);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <Router>
+            {/*<App/>*/}
+            <Test/>
+        </Router>
+    </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
